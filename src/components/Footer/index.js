929e18a4fae98669692d0eadf14d2a29f70a240c
Alexioclock <alexisbertell@gmail.com>
2021-10-26 10:52:32 +0200

    feature footer : doing

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+// == Composant
+const Footer = () => (
+  <div className="footer">
+    <p>footer</p>
+  </div>
+);
+
+// == Export
+export default Footer;
