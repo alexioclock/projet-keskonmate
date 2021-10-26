7692e5822d52e7e7107d35c7879b1f534ec668c5
wucéline <wuwuceline@gmail.com>
2021-10-26 15:36:51 +0200

    changing project background color + adding vars colors

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 // == Import
 import './styles.scss';
+import Header from 'src/components/Header';
 
 // == Composant
 const App = () => (
   <div className="app">
-
+    <Header />
   </div>
 );
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Header = () => (
+  <div>
+    <p className="tryfont">
+      Hey
+    </p>
+  </div>
+);
+
+export default Header;
