ffa05e87e1512246794bbb72cafe8764d1121145
wucéline <wuwuceline@gmail.com>
2021-10-26 17:34:01 +0200

    added search bar + react feather

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,43 @@
 import './styles.scss';
+import { User, Search } from 'react-feather';
 
 const Header = () => (
   <div className="header">
-    <a href="/" className="link_series">
-      Toutes les séries
-    </a>
+    <div className="left-navbar">
+      <a href="/series" className="series-link">
+        Toutes les séries
+      </a>
+
+      <form className="form">
+        <input className="form-input"
+          placeholder="Rechercher une série"
+        />
+        <button
+          type="submit"
+          className="form-submit"
+        >
+          <Search size="20" />
+        </button>
+      </form>
+    </div>
 
     <h1 className="logo">
       KESKONMATE ?
     </h1>
 
-    <a href="/" className="link_list">
-      Mes listes
-    </a>
+    <div className="right-navbar">
+      <a href="/list" className="list-link">
+        Mes listes
+      </a>
+
+      <p className="user-name">
+        Utilisateur
+      </p>
+
+      <a href="/user" className="user-account-link">
+        <User className="user-icon" />
+      </a>
+    </div>
 
   </div>
 );
