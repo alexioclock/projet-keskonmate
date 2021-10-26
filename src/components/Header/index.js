fbb4a89ee8c49b1f28206936fa3543d7d0862487
wucéline <wuwuceline@gmail.com>
2021-10-26 16:06:37 +0200

    added links series and lists

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,18 @@ import './styles.scss';
 
 const Header = () => (
   <div className="header">
+    <a href="/" className="link_series">
+      Toutes les séries
+    </a>
+
     <h1 className="logo">
       KESKONMATE ?
     </h1>
+
+    <a href="/" className="link_list">
+      Mes listes
+    </a>
+
   </div>
 );
 
