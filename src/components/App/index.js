ba90fcb452ac872b7289d1396372937119b4fa9a
Dorian Lalanne <dorian.lalanne@yahoo.fr>
2021-10-28 16:49:42 +0200

    static integration and responsive : DONE

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,12 @@
 // == Import
 import './styles.scss';
 import Header from 'src/components/Header';
-import Catalogue from 'src/components/Catalogue';
-import Detail from '../Detail';
-import Form from '../Form';
-import Footer from '../Footer';
-import NotFound from '../NotFound'
-
-
+import ProfilePage from 'src/components/ProfilePage';
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
-    <Catalogue />
-    <NotFound />
-    <Footer />
+    <ProfilePage />
   </div>
 );
 
--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,1 +1,43 @@
-console.log('init');
+// Composants
+
+// Style
+import './styles.scss';
+
+// == Composant
+const ProfilePage = () => (
+  <div className="profile-page">
+    <h1 className="profile-page-title">Mon profil</h1>
+    <div className="profile-page-container">
+      <h2 className="profile-page-subtitle">Mon pseudo</h2>
+      {/* value permet de préremplir le champ avec les données de l'utilisateur :
+      TODO le rendre dynamique avec les données reçues */}
+      <p>Pseudo</p>
+    </div>
+    <div className="profile-page-container">
+      <h2 className="profile-page-subtitle">Mon email</h2>
+      {/* value permet de préremplir le champ avec les données de l'utilisateur :
+      TODO le rendre dynamique avec les données reçues */}
+      <p>example@keskonmate.io</p>
+    </div>
+    {/* Il faudra y mettre la route vers l'API qui édite un utilisateur */}
+    <form action="/" method="post" className="profile-page-container">
+      <h2 className="form-title profile-page-subtitle">Je veux changer mon mot de passe</h2>
+      <div className="old-password-container">
+        <h3 className="old-password-title form-subtitle">Saisir l'ancien mot de passe</h3>
+        <input type="password" id="password" name="password" placeholder="Ancien mot de passe" />
+      </div>
+      <div className="password-container">
+        <h3 className="password-title form-subtitle">Saisir le nouveau mot de passe</h3>
+        <input type="password" id="password" name="password" placeholder="Nouveau mot de passe" />
+      </div>
+      <div className="password-validation-container">
+        <h3 className="password-validation-title form-subtitle">Confirmer le nouveau mot de passe</h3>
+        <input type="password" id="password-validation" name="password-validation" placeholder="Confirmer" />
+      </div>
+      <button className="validation-button" type="submit">Valider</button>
+    </form>
+  </div>
+);
+
+// == Export
+export default ProfilePage;
