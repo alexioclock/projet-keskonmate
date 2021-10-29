9afd767d0498168e475953ffea2eaca06e9aa61a
Dorian Lalanne <dorian.lalanne@yahoo.fr>
2021-10-29 12:16:26 +0200

    creation static data utils to implement on components : DONE

--- src/utils/genres.js
+++ src/utils/genres.js
@@ -2,3 +2,30 @@
  On veut afficher les genres dans :
   - page catalogue : dropdown
 */
+
+export default [
+  {
+    id: 1,
+    name: 'Drame',
+  },
+  {
+    id: 2,
+    name: 'Fantastique',
+  },
+  {
+    id: 3,
+    name: 'Humour',
+  },
+  {
+    id: 4,
+    name: 'Thriller',
+  },
+  {
+    id: 5,
+    name: 'Science-Fiction',
+  },
+  {
+    id: 6,
+    name: 'Animation',
+  },
+];
---
+++ src/utils/userlists.js
@@ -0,0 +1,86 @@
+export default [
+  // Déjà vus (1) : GoT, The Office, The Big Bang Theory
+  // En cours (2) : La Casa de Papel
+  // A voir (3) : Squid Game
+  // Pas dans une liste : Death Note
+  {
+    id: 1,
+    seasonNb: null,
+    seriesNb: 1,
+    episodeNb: null,
+    createdAt: '2021-10-27T12:40:34+02:00',
+    updatedAt: null,
+    // 1 = Already watched, 2 = Watching, 3 = To watch
+    type: 1,
+    series: [
+      {
+        id: 1,
+        title: 'Game of Thrones',
+      },
+    ],
+  },
+  {
+    id: 2,
+    seasonNb: null,
+    seriesNb: 2,
+    episodeNb: null,
+    createdAt: '2021-10-27T12:40:34+02:00',
+    updatedAt: null,
+    // 1 = Already watched, 2 = Watching, 3 = To watch
+    type: 1,
+    series: [
+      {
+        id: 2,
+        title: 'The Office',
+      },
+    ],
+  },
+  {
+    id: 3,
+    seasonNb: null,
+    seriesNb: 4,
+    episodeNb: null,
+    createdAt: '2021-10-27T12:40:34+02:00',
+    updatedAt: null,
+    // 1 = Already watched, 2 = Watching, 3 = To watch
+    type: 1,
+    series: [
+      {
+        id: 4,
+        title: 'The Big Bang Theory',
+      },
+    ],
+  },
+  {
+    id: 4,
+    seasonNb: null,
+    seriesNb: 5,
+    episodeNb: null,
+    createdAt: '2021-10-27T12:40:34+02:00',
+    updatedAt: null,
+    // 1 = Already watched, 2 = Watching, 3 = To watch
+    type: 3,
+    series: [
+      {
+        id: 5,
+        title: 'Squid Game',
+      },
+    ],
+  },
+  {
+    id: 5,
+    seasonNb: null,
+    seriesNb: 3,
+    episodeNb: null,
+    createdAt: '2021-10-27T12:40:34+02:00',
+    updatedAt: null,
+    // 1 = Already watched, 2 = Watching, 3 = To watch
+    type: 2,
+    series: [
+      {
+        id: 3,
+        title: 'La Casa de Papel',
+      },
+    ],
+  },
+];
