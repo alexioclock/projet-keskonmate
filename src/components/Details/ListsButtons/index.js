b05c38e9985f8e507ed6da59725bf1b2dbf2b8b8
wucéline <wuwuceline@gmail.com>
2021-11-03 17:04:56 +0100

    return if type=1 in ListsButtons

--- src/components/Details/ListsButtons/index.js
+++ src/components/Details/ListsButtons/index.js
@@ -1,11 +1,34 @@
 import './styles.scss';
 import PropTypes from 'prop-types';
 
-const ListsButtons = ({ watched }) => {
-  console.log(watched);
+const ListsButtons = ({ type }) => {
+  if (type === 1) {
+    return (
+      <div className="type">
+        <p className="list-name">Déjà vu</p>
+        <a href="#" className="button">Revoir </a>
+        <a href="#" className="button">Supprimer </a>
+      </div>
+    );
+  }
+  if (type === 2) {
+    return (
+      <div>
+        type=2
+      </div>
+    );
+  }
+
+  if (type === 2) {
+    return (
+      <div>
+        type=2
+      </div>
+    );
+  }
   return (
     <div>
-
+      <a className="add-list-button">A voir</a>
     </div>
   );
 };
