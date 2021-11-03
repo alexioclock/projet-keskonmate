540fa6b3c273021edf55d75930cebdae99a8010a
Alexioclock <alexisbertell@gmail.com>
2021-11-03 16:48:39 +0100

    fixing router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,6 +10,9 @@ import Footer from '../Footer';
 import NotFound from '../NotFound';
 import PageList from '../PageList';
 import Home from '../Home';
+import WatchedSeries from 'src/containers/SeriesList/WatchedSeries';
+import ToWatchSeries from 'src/containers/SeriesList/ToWatchSeries';
+import CurrentSeries from 'src/containers/SeriesList/CurrentSeries';
 
 // == Composant
 const App = () => {
@@ -19,6 +22,9 @@ const App = () => {
     <Header />
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/series-vues" exact component={WatchedSeries} />
+      <Route path="/series-en-cours" exact component={CurrentSeries} />
+      <Route path="/series-a-voir" exact component={ToWatchSeries} />
       <Route path="/series" exact component={Catalogue} />
       <Route
         path="/series/nom"
@@ -29,6 +35,7 @@ const App = () => {
       <Route path="/mon-compte" exact component={ProfilePage} />
       <Route path="/inscription" exact component={Form} />
       <Route component={NotFound} />
+
     </Switch>
     <Footer />
   </BrowserRouter>
--- src/components/PageList/index.js
+++ src/components/PageList/index.js
@@ -1,22 +1,14 @@
 // == Import
 import './styles.scss';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import WatchedSeries from 'src/containers/SeriesList/WatchedSeries';
-import ToWatchSeries from 'src/containers/SeriesList/ToWatchSeries';
 import CurrentSeries from 'src/containers/SeriesList/CurrentSeries';
 import ListNavigation from './ListNavigation';
 
 // == Composant
 const PageList = () => (
   <div className="list-navigation">
-    <Router>
       <ListNavigation />
-      <Route path="/series-vues" exact component={WatchedSeries} />
-      <Route path="/series-en-cours" exact component={CurrentSeries} />
-      <Route path="/series-a-voir" exact component={ToWatchSeries} />
       <CurrentSeries />
-    </Router>
   </div>
 );
 
