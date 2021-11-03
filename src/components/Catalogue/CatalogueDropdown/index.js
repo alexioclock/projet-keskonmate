0ab67a106c017fcc85e1ea0c8535cd0cc119be98
Alexioclock <alexisbertell@gmail.com>
2021-11-03 10:48:57 +0100

    map genre dropdown ok

--- src/components/Catalogue/CatalogueDropdown/index.js
+++ src/components/Catalogue/CatalogueDropdown/index.js
@@ -6,17 +6,6 @@ import './styles.scss';
 
 const CatalogueDropdown = ({genre}) => {
 
-console.log(genre[0].name);
-const options = [
-  
-  { key: (genre[0].name), text: (genre[0].name), value: (genre[0].name) },
-  { key: (genre[1].name), text: (genre[1].name), value: (genre[1].name) },
-  { key: (genre[2].name), text: (genre[2].name), value: (genre[2].name) },
-  { key: (genre[3].name), text: (genre[3].name), value: (genre[3].name) },
-  { key: (genre[4].name), text: (genre[4].name), value: (genre[4].name) },
-  { key: (genre[5].name), text: (genre[5].name), value: (genre[5].name) },
-];
-
 
 return(
   <div className="catalogue-dropdown">
@@ -25,8 +14,9 @@ return(
       fluid
       multiple
       selection
-      options={options}
-
+      options= {genre.map((item) => (
+        { key: (item.name), text: (item.name), value: (item.name) }
+      ))}
     />
   </div>
 );
