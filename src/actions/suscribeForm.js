e503973717494f34a5577be7251829ac770c7bc3
Alexioclock <alexisbertell@gmail.com>
2021-11-05 14:23:08 +0100

    form redux wip

--- src/actions/suscribeForm.js
+++ src/actions/suscribeForm.js
@@ -1,4 +1,9 @@
 export const SET_INPUT_NICKNAME = 'SET_INPUT_NICKNAME';
+export const SET_EMAIL_INPUT = 'SET_EMAIL_INPUT';
+export const SET_PASSWORD_INPUT = 'SET_PASSWORD_INPUT';
+export const SET_CONFIRM_PASSWORD_INPUT= 'SET_CONFIRM_PASSWORD_INPUT';
+export const SUCCESS_SUSCRIBE= 'SUCCESS_SUSCRIBE';
+
 
 // === action creators
 export const setInputNickname = (newValue) => ({
@@ -7,7 +12,6 @@ export const setInputNickname = (newValue) => ({
 });
 
 
-export const SET_EMAIL_INPUT = 'SET_EMAIL_INPUT';
 
 // === action creators
 export const setEmailInput = (newValue) => ({
@@ -15,7 +19,6 @@ export const setEmailInput = (newValue) => ({
   value: newValue,
 });
 
-export const SET_PASSWORD_INPUT = 'SET_PASSWORD_INPUT';
 
 // === action creators
 export const setPasswordInput = (newValue) => ({
@@ -23,10 +26,9 @@ export const setPasswordInput = (newValue) => ({
   value: newValue,
 });
 
-export const SET_CONFIRM_PASSWORD_INPUT= 'SET_CONFIRM_PASSWORD_INPUT';
 
 // === action creators
-export const setConfirmPasswordInput = (newValue) => ({
-  type: SET_CONFIRM_PASSWORD_INPUT,
-  value: newValue,
+export const successSuscribe = () => ({
+  type: SUCCESS_SUSCRIBE,
 });
+
