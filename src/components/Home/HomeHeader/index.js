fa63ee39bc6adb5efac290a6bfee77d176a3cc0a
wucéline <wuwuceline@gmail.com>
2021-11-05 17:23:46 +0100

    display loginForm onClick on connectez vous button

--- src/components/Home/HomeHeader/index.js
+++ src/components/Home/HomeHeader/index.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
-
+import { useState } from 'react';
 import './styles.scss';
+import LoginForm from 'src/components/Header/LoginForm';
 
-const HomeHeader = ({ isConnected, username }) => (
-  <div className="home-header">
-    {!isConnected && (
+const HomeHeader = ({ isConnected, username }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  return (
+    <div className="home-header">
+      {!isConnected && (
       <div className="home-header-not-connected">
         <div className="home-header-not-connected-text">
           <p className="line-one">
             Bienvenue sur keskonmate !
           </p>
           <p className="line-two">
-            Un site qui a été pensé pour gérer le visionnage de vos séries 
+            Un site qui a été pensé pour gérer le visionnage de vos séries
             et créer vos propres listes.
           </p>
         </div>
@@ -23,13 +26,22 @@ const HomeHeader = ({ isConnected, username }) => (
           <p>
             ou
           </p>
-          <a href="/login">
+          <button
+            type="button"
+            className="connection"
+            onClick={() => {
+              setIsDropdownOpen(!isDropdownOpen);
+            }}
+          >
             Connectez-vous
-          </a>
+          </button>
         </div>
+        {isDropdownOpen && (
+        <LoginForm />
+        )}
       </div>
-    )}
-    {isConnected && (
+      )}
+      {isConnected && (
       <div className="home-header-connected">
         <p className="home-header-connected-first-line">
           Salut {username} !
@@ -38,10 +50,10 @@ const HomeHeader = ({ isConnected, username }) => (
           Alors, keskonmate ?
         </p>
       </div>
-    )}
-  </div>
-);
-
+      )}
+    </div>
+  );
+};
 HomeHeader.propTypes = {
   isConnected: PropTypes.bool.isRequired,
   username: PropTypes.string.isRequired,
