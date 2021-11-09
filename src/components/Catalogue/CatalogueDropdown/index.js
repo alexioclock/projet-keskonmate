3e8fe7c65fbca1bca410a6c234c8097cbe901694
Alexioclock <alexisbertell@gmail.com>
2021-11-09 10:09:52 +0100

    fetch genre with API with dropdownf

--- src/components/Catalogue/CatalogueDropdown/index.js
+++ src/components/Catalogue/CatalogueDropdown/index.js
@@ -1,4 +1,8 @@
 import { Dropdown } from 'semantic-ui-react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
 
 // Style
 import './styles.scss';
@@ -6,7 +10,24 @@ import './styles.scss';
 
 const CatalogueDropdown = ({genre}) => {
 
+  const [currentGenre, setCurrentGenre] = useState([]);
 
+
+  useEffect(() => {
+    axios.get(`http://keskonmate.me/api/v1/genres`)
+      .then((response) => {
+        console.log(response.data[0].name);
+         setCurrentGenre(response.data);
+        // setIsLoading(false);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }, []);
+
+
+  console.log(currentGenre);
+  
 return(
   <div className="catalogue-dropdown">
     <Dropdown
@@ -14,8 +35,8 @@ return(
       fluid
       multiple
       selection
-      options= {genre.map((item) => (
-        { key: (item.name), text: (item.name), value: (item.name) }
+      options= {currentGenre.map((item) => (
+        { key: (item.id), text: (item.name), value: (item.name) }
       ))}
     />
   </div>
